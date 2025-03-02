3d64ee4a21bb6a3246cd274a7de8ffc8b295a385
Marijonhel latosa <latosamarijonhel_bsit@plmun.edu.ph>
2025-03-02 19:12:21 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -7,14 +7,4 @@ USE `UniversityDB`;
 -- Verification (Optional): 
 -- You can add a simple query to verify the database is selected.
 SELECT DATABASE(); -- This will show the currently selected database.
-Use `UniversityDB`;
-
--- Step 1: Create the Students table with constraints
-CREATE TABLE `Students` (
-   `StudentID` INT PRIMARY KEY AUTO_INCREMENT,  -- MySQL Example: AUTO_INCREMENT
-   -- For other databases (e.g., PostgreSQL, SQL Server) you might use SERIAL or IDENTITY
-   `FirstName` VARCHAR(50) NOT NULL,
-   `LastName` VARCHAR(50) NOT NULL,
-   `EnrollmentDate` DATE
-);
 
